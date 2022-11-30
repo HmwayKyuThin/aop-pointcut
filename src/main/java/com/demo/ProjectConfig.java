7c172fa004fa88aa21272e07cc95a4e5daf33e3f
HmwayKyuThin <hmwaykyu92@gmail.com>
2022-11-30 10:33:43 +0630

    first commit

---
+++ src/main/java/com/demo/ProjectConfig.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan
+@EnableAspectJAutoProxy
+public class ProjectConfig {
+}
